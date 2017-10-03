59f9625ee592ed63a0c922b55a52dff25c63882f
marcinkociolek <marcin.kociolek@gmail.com>
2017-10-03 19:29:10 +0200

    Tile statistics class aded

--- LibMarcin/tileparams.cpp
+++ LibMarcin/tileparams.cpp
@@ -2,6 +2,18 @@
 #include <iostream>
 #include <sstream>
 #include <vector>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+
+#include <boost/filesystem.hpp>
+#include <boost/regex.hpp>
+
+//#include <opencv2/core/core.hpp>
+
+using namespace std;
+using namespace boost::filesystem;
+using namespace boost;
+using namespace cv;
 
 TileParams::TileParams()
 {
@@ -76,3 +88,35 @@ void TileParams::FromString(std::string InStr)
      NamesVector.empty();
      ParamsVect.empty();
  }
+ //-----------------------------------------------------------------------------------------------------------
+ void IntensityStatistics::SaveToFile(string Filename)
+ {
+     std::string OutString ="";
+     OutString +="Fie name \t" + Filename + "\n";
+     OutString +="tile Y \t" + to_string(tileY) + "\n";
+     OutString +="tile X \t" + to_string(tileY) + "\n";
+     OutString +="pixel Count \t" + to_string(pixelCount) + "\n";
+     OutString +="min \t" + to_string(minVal) + "\n";
+     OutString +="max \t" + to_string(minVal) + "\n";
+     OutString +="mean\t" + to_string(meanVal) + "\n";
+     OutString +="stdVal \t" +to_string(stdVal) + "\n";
+     for (int i = 0; i < 2048; i++)
+     {
+         OutString +=to_string(i*32)+"\t" + to_string(Hist[i]) + "\n";
+     }
+ }
+ //-----------------------------------------------------------------------------------------------------------
+ void IntensityStatistics::FromFile(string FileNameExt)
+{
+   Mat Image;
+   Image = imread(FileNameExt,CV_LOAD_IMAGE_ANYDEPTH);
+   FileName = FileNameExt;
+   int pixelCount;
+   int *Hist;
+   int minVal;
+   int maxVal;
+   int modeVal;
+   double meanVal;
+   double stdVal;
+   FileName;
+}
--- LibMarcin/tileparams.h
+++ LibMarcin/tileparams.h
@@ -5,6 +5,9 @@
 #include <vector>
 #include <boost/filesystem.hpp>
 
+#include <opencv2/core.hpp>
+#include <boost/regex.hpp>
+
 class TileParams
 {
 public:
@@ -37,6 +40,50 @@ public:
     FileParams();
     ~FileParams();
 
+};
+
+class IntensityStatistics
+{
+public:
+    int tileY;
+    int tileX;
+    int pixelCount;
+    int *Hist;
+    int minVal;
+    int maxVal;
+    int modeVal;
+    double meanVal;
+    double stdVal;
+    std::string FileName;
+
+    IntensityStatistics()
+    {
+        Init();
+    }
+
+    void Init()
+    {
+        tileY = -1;
+        tileX = -1;
+        pixelCount = 0;
+        Hist = new int[2048];
+        for(int i = 0; i <2048 ;i++)
+            Hist[i] = 0;
+        minVal = 65536;
+        maxVal = 0;
+        modeVal = 0;
+        meanVal = 0;
+        stdVal = 0;
+        FileName.empty();
+    }
+    ~IntensityStatistics()
+    {
+        delete[] Hist;
+        Hist = 0;
+        FileName.empty();
+    }
+    void SaveToFile(std::string FileMameOut);
+    void FromFile(std::string FileNameIn);
 };
 
 #endif // TILEPARAMS_H
