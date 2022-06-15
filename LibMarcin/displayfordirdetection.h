74d392e7058319099bf5edb4dd24024ed6a795c6
Marcin Kociołek <marcin.kociolek@gmail.com>
2022-06-15 18:24:03 +0200

    ShowDirectionGray added

--- LibMarcin/displayfordirdetection.h
+++ LibMarcin/displayfordirdetection.h
@@ -1,9 +1,13 @@
 #ifndef DISPLAYFORDIRDETECTION_H
 #define DISPLAYFORDIRDETECTION_H
+#include <opencv2/core/core.hpp>
+#include "dirdetectionparams.h"
 
 void DrawTilesOnImage(cv::Mat ImIn, DirDetectionParams params);
 void DrawTilesOnImage(cv::Mat ImIn, bool showTilesOnImage, int tileShape, int tileOffsetX, int inttileOffsetY,
                       int tileSize, int tileShift, int tileLineWidth);
-void ShowDirection(cv::Mat ImToShow, int y, int x, float direction, int lineWidth, int lineLength, double scale = 1.0);
+void ShowDirection(cv::Mat ImToShow, int y, int x, float direction, int lineWidth, int lineLength, double scale = 1.0 , cv::Scalar color = cv::Scalar(0.0, 0.0, 0.0, 0.0));
+void ShowDirectionGray(cv::Mat ImToShow, int y, int x, float direction, int lineWidth, int lineLength, double scale = 1.0 , cv::Scalar color = cv::Scalar(0.0, 0.0, 255.0, 0.0));
+
 
 #endif // DISPLAYFORDIRDETECTION_H
